47fcdb27cfb37d14d1ec86e107ed149cc3983429
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-04-08 20:49:17 -0500

    pushing changes to the main branch for the user to read and delete author cards

--- pages/author/authors.js
+++ pages/author/authors.js
@@ -1,5 +1,40 @@
-// import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getAuthors } from '../../api/authorData';
+import { useAuth } from '../../utils/context/authContext';
+import AuthorCard from '../../components/forms/AuthorCard';
 
-// export default function Author() {
-//  return <div>author-route</div>;
-// }
+function Authors() {
+  // Set a state for authors
+  const [authors, setAuthors] = useState([]);
+
+  // Get user ID using useAuth Hook
+  const { user } = useAuth();
+
+  //  create a function that makes the API call to get all the authors
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+
+  // make the call to the API to get all the authors on component render
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/book/new" passHref>
+        <Button>Add A Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {authors.map((auth) => (
+          <AuthorCard key={auth.firebaseKey} authObj={auth} onUpdate={getAllTheAuthors} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default Authors;
--- pages/author/new.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// TODO: Add author form
-
-export default function AuthorForm() {
-  return <div>Add Author Form</div>;
-}
